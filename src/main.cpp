0bd8b7638e720f11d3b2a562ef69b1df4dde6167
vas59 <vasily.yuryev.a1@gmail.com>
2019-08-15 13:55:53 +0300

    navigate

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,12 @@ PubSubClient client(wifiClient);
 
 const char *ssid = "........";
 const char *password = "........";
-const char *mqtt_server = "broker.mqtt-dashboard.com";
+const char *mqtt_server = "broker_swarm.local";
 
 Drone *drone;
+
+String mac_addres = "tester";
+String navigate_topic = String("drones/") + mac_addres + "/navigate";
 
 /*
 {
@@ -42,7 +45,7 @@ Drone *drone;
         "yaw":0.0
     },
    "mode":"",
-    "armed":true,
+   "armed":true,
    "connected":true
 
 }
@@ -64,9 +67,9 @@ void telemetry_to_jsonobject(DynamicJsonDocument &jsonBuffer, Telemetry telemetr
     velocity["z"] = telemetry.velocity.z;
 
     JsonObject rotation = jsonBuffer.createNestedObject("rotation");
-    rotation["roll"] = 0.2;
-    rotation["pitch"] = 0.2;
-    rotation["yaw"] = 0.2;
+    rotation["roll"] = telemetry.rotation.roll;
+    rotation["pitch"] = telemetry.rotation.pitch;
+    rotation["yaw"] = telemetry.rotation.yaw;
 
     jsonBuffer["mode"] = mode_ToString(telemetry.mode).c_str();
     jsonBuffer["armed"] = telemetry.armed;
@@ -74,8 +77,50 @@ void telemetry_to_jsonobject(DynamicJsonDocument &jsonBuffer, Telemetry telemetr
     // return root;
 }
 
+/*
+
+{
+    "position":{
+        "x":0.1,
+        "y":0.1,
+        "z":0.1,
+        "yaw":0.1
+    },
+    "speed":0.5,
+    "led":{
+        "r":255,
+        "g":255,
+        "b":255
+    }
+}
+
+*/
+
 void callback(char *topic, byte *payload, unsigned int length)
 {
+    if (String(topic) == navigate_topic)
+    {
+        const size_t capacity = 2 * JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(4) + 50;
+        DynamicJsonDocument doc(capacity);
+        deserializeJson(doc, payload, length);
+
+        JsonObject position = doc["position"];
+        float position_x = position["x"];     // 0.1
+        float position_y = position["y"];     // 0.1
+        float position_z = position["z"];     // 0.1
+        float position_yaw = position["yaw"]; // 0.1
+
+        float speed = doc["speed"];
+        // float speed = doc["speed"];
+
+        JsonObject led = doc["led"];
+        int led_r = led["r"]; // 255
+        int led_g = led["g"]; // 255
+        int led_b = led["b"]; // 255
+
+        // drone->navigate({position_x, position_y, position_z, position_yaw}, FRAME_LOCAL, speed);
+        drone->set_position({position_x, position_y, position_z, position_yaw}, FRAME_LOCAL);
+    }
 }
 
 void setup()
@@ -89,6 +134,7 @@ void setup()
         delay(500);
         Serial.print(".");
     }
+    mac_addres = WiFi.macAddress();
 
     client.setServer(mqtt_server, 1883);
     client.setCallback(callback);
@@ -100,6 +146,8 @@ void setup()
     drone = new Drone(&ai);
     drone->start();
 
+    navigate_topic = String("drones/") + mac_addres + "/navigate";
+    client.subscribe(navigate_topic.c_str());
     // drone->arm();
     // drone->sleep(1000);
     // drone->set_position({0, 0, 2, 0}, FRAME_LOCAL);
@@ -125,21 +173,28 @@ void setup()
     // mavlink_msg_heartbeat_decode(&msg, &hrbt);
 }
 unsigned long prev_time = 0;
+
+void publish_telemetry()
+{
+    const size_t capacity = 3 * JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(6);
+    DynamicJsonDocument jsonBuffer(capacity);
+
+    telemetry_to_jsonobject(jsonBuffer, drone->get_telemetry(FRAME_LOCAL));
+
+    String json_p("");
+    serializeJsonPretty(jsonBuffer, json_p);
+    Serial.println(json_p);
+
+    String telemetry_topic = String("drones/") + mac_addres + "/telemetry";
+    client.publish(telemetry_topic.c_str(), json_p.c_str());
+}
+
 void loop()
 {
-    if ((millis() - prev_time) >= 500)
+    if ((millis() - prev_time) >= 200)
     {
-        Serial.println(drone->get_telemetry(FRAME_LOCAL).ToString());
-
-        const size_t capacity = 3 * JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(6);
-        DynamicJsonDocument jsonBuffer(capacity);
-
-        telemetry_to_jsonobject(jsonBuffer, drone->get_telemetry(FRAME_LOCAL));
-
-        String json_p("");
-        serializeJsonPretty(jsonBuffer, json_p);
-
-        client.publish("telemetry", json_p.c_str());
+        // Serial.println(drone->get_telemetry(FRAME_LOCAL).ToString());
+        publish_telemetry();
         prev_time = millis();
     }
     client.loop();
